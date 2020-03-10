f1b3f2ff9d1c9418e0bdc577b948328dd3cac140
airingdeng <airingdeng@tencent.com>
2020-03-10 16:48:05 +0800

    Initial Commit

---
+++ mlive_iosTests/mlive_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  mlive_iosTests.swift
+//  mlive_iosTests
+//
+//  Created by 邓国雄 on 2020/3/10.
+//  Copyright © 2020 邓国雄. All rights reserved.
+//
+
+import XCTest
+@testable import mlive_ios
+
+class mlive_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
