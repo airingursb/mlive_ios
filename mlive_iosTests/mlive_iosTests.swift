1c26d8c7446c3b99f9ca3a5ca9b34cd36166b26d
airingdeng <airingdeng@tencent.com>
2020-03-17 19:23:48 +0800

    feat: 接入常规 Flutter 混合开发demo

--- mlive_iosTests/mlive_iosTests.swift
+++ mlive_iosTests/mlive_iosTests.swift
@@ -2,7 +2,7 @@
 //  mlive_iosTests.swift
 //  mlive_iosTests
 //
-//  Created by 邓国雄 on 2020/3/10.
+//  Created by 邓国雄 on 2020/3/17.
 //  Copyright © 2020 邓国雄. All rights reserved.
 //
 
--- mlive_iosUITests/mlive_iosUITests.swift
+++ mlive_iosUITests/mlive_iosUITests.swift
@@ -2,7 +2,7 @@
 //  mlive_iosUITests.swift
 //  mlive_iosUITests
 //
-//  Created by 邓国雄 on 2020/3/10.
+//  Created by 邓国雄 on 2020/3/17.
 //  Copyright © 2020 邓国雄. All rights reserved.
 //
 
